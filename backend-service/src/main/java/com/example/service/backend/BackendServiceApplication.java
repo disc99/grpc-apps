1d0905695bbff497f8384f1bfa2982d1608c4779
d.kanehira <disc99.o@gmail.com>
2018-11-19 00:26:24 +0900

    Add error handle

--- backend-service/src/main/java/com/example/service/backend/BackendServiceApplication.java
+++ backend-service/src/main/java/com/example/service/backend/BackendServiceApplication.java
@@ -1,16 +1,33 @@
 package com.example.service.backend;
 
-import com.google.rpc.DebugInfo;
-import io.grpc.*;
+import com.google.protobuf.Any;
+import com.google.rpc.BadRequest;
+import com.google.rpc.Status;
+import io.grpc.ServerInterceptor;
+import io.grpc.StatusRuntimeException;
 import io.grpc.examples.helloworld.GreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
-import io.grpc.protobuf.ProtoUtils;
+import io.grpc.protobuf.StatusProto;
 import io.grpc.stub.StreamObserver;
+import io.grpc.util.TransmitStatusRuntimeExceptionInterceptor;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.lognet.springboot.grpc.GRpcGlobalInterceptor;
 import org.lognet.springboot.grpc.GRpcService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.google.rpc.Code.INTERNAL_VALUE;
+import static com.google.rpc.Code.INVALID_ARGUMENT_VALUE;
+import static com.google.rpc.Code.NOT_FOUND_VALUE;
 
 @SpringBootApplication
 public class BackendServiceApplication {
@@ -20,93 +37,125 @@ public class BackendServiceApplication {
 	}
 
 	@GRpcService
-	public static class GreeterService extends  GreeterGrpc.GreeterImplBase {
-		@Value("${grpc.port}")
-		int gport;
+	static class GreeterService extends GreeterGrpc.GreeterImplBase {
+
+		GreeterLogic logic;
+
+		GreeterService(GreeterLogic logic) {
+			this.logic = logic;
+		}
 
 		@Override
 		public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-			HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
+			HelloReply reply = logic.findBy(request);
+
 			responseObserver.onNext(reply);
 			responseObserver.onCompleted();
-		}
-
-		@Override
-		public void sayHelloUnary(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-//			Metadata metadata = new Metadata();
-//			Metadata.Key<DebugInfo> key = ProtoUtils.keyForProto(DebugInfo.getDefaultInstance());
-//			DebugInfo values = DebugInfo.newBuilder()
-//					.addStackEntries("stack_entry_1")
-//					.addStackEntries("stack_entry_2")
-//					.addStackEntries("stack_entry_3")
-//					.setDetail("detailed error info.").build();
-//			metadata.put(key, values);
-//			StatusRuntimeException error = Status.INVALID_ARGUMENT
-//					.withDescription("ERROR DESC")
-//					.asRuntimeException(metadata);
-//			throw error;
-//            responseObserver.onError(error);
-
-            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()
-//                    + " server.port="+port
-                    + " grpc.port=" + gport).build();
-            responseObserver.onNext(reply);
-            responseObserver.onCompleted();
-		}
-
-		@Override
-		public void sayHelloServerStreaming(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-			HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
-			responseObserver.onNext(reply);
-			responseObserver.onNext(reply);
-			responseObserver.onNext(reply);
-			responseObserver.onCompleted();
-		}
-
-		@Override
-		public StreamObserver<HelloRequest> sayHelloClientStreaming(StreamObserver<HelloReply> responseObserver) {
-			return new StreamObserver<HelloRequest>() {
-				@Override
-				public void onNext(HelloRequest request) {
-					HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
-					responseObserver.onNext(reply);
-					responseObserver.onNext(reply);
-					responseObserver.onNext(reply);
-					responseObserver.onCompleted();
-				}
-				@Override
-				public void onError(Throwable t) {
-					// ...
-				}
-				@Override
-				public void onCompleted() {
-					// ...
-				}
-			};
-		}
-
-		@Override
-		public StreamObserver<HelloRequest> sayHelloBidirectionalStreaming(StreamObserver<HelloReply> responseObserver) {
-			return new StreamObserver<HelloRequest>() {
-				@Override
-				public void onNext(HelloRequest request) {
-					System.out.println("Call onNext");
-					HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
-					responseObserver.onNext(reply);
-					responseObserver.onNext(reply);
-					responseObserver.onNext(reply);
-					responseObserver.onCompleted();
-				}
-				@Override
-				public void onError(Throwable t) {
-					// ...
-				}
-				@Override
-				public void onCompleted() {
-					// ...
-				}
-			};
 		}
 	}
 
+	@Service
+	static class GreeterLogic {
+        public HelloReply findBy(HelloRequest request) {
+            String name = request.getName();
+
+            if ("400".equals(name)) {
+                throw new InvalidException("Invalid!!",
+						Arrays.asList("Require name", "Only over 20"));
+            }
+
+            if ("404".equals(name)) {
+                throw new ResourceNotFoundException("Not found!!");
+            }
+
+            if ("500".equals(name)) {
+                throw new InternalServerError("Internal error!!");
+            }
+
+            if ("native".equals(name)) {
+                throw new RuntimeException("Native exception!!");
+            }
+
+            return HelloReply.newBuilder()
+                    .setMessage("Hello " + request.getName())
+                    .setDescription("Desc: " + request.getNickname())
+                    .build();
+        }
+    }
+
+	static class InternalServerError extends RuntimeException {
+        InternalServerError(String message) {
+            super(message);
+        }
+    }
+
+	static class InvalidException extends RuntimeException {
+		List<String> violations;
+        InvalidException(String message, List<String> violations) {
+            super(message);
+            this.violations = violations;
+        }
+    }
+
+	static class ResourceNotFoundException extends RuntimeException {
+        ResourceNotFoundException(String message) {
+            super(message);
+        }
+    }
+
+	@Aspect
+    @Component
+    static class ApplicationAdvice {
+
+        @AfterThrowing(value="execution(* com.example.service.backend..*.*(..))", throwing="e")
+        public void handle(RuntimeException e) {
+            String message = e.getMessage();
+            String errorMessage = message == null ? e.toString() : message;
+
+            if (e instanceof ResourceNotFoundException) {
+                throw toStatusRuntimeException(NOT_FOUND_VALUE, errorMessage, null);
+            }
+
+            if (e instanceof InvalidException) {
+                BadRequest badRequest = ((InvalidException) e).violations.stream()
+                        .map(this::toFieldViolation)
+                        .collect(Collectors.collectingAndThen(Collectors.toList(), this::toBadRequest));
+                throw toStatusRuntimeException(INVALID_ARGUMENT_VALUE, errorMessage, badRequest);
+            }
+
+            if (e instanceof InternalServerError) {
+                throw toStatusRuntimeException(INTERNAL_VALUE, errorMessage, null);
+            }
+
+            throw toStatusRuntimeException(INTERNAL_VALUE, errorMessage, null);
+        }
+
+        private BadRequest toBadRequest(List<BadRequest.FieldViolation> violations) {
+            return BadRequest.newBuilder().addAllFieldViolations(violations).build();
+        }
+
+        private BadRequest.FieldViolation toFieldViolation(String violation) {
+            return BadRequest.FieldViolation.newBuilder().setDescription(violation).build();
+        }
+
+        private <T extends com.google.protobuf.Message> StatusRuntimeException toStatusRuntimeException(int code, String message, T details) {
+            Status.Builder builder = Status.newBuilder()
+                    .setCode(code)
+                    .setMessage(message);
+
+            if (details != null) {
+                builder.addDetails(Any.pack(details));
+            }
+
+            Status status = builder.build();
+            return StatusProto.toStatusRuntimeException(status);
+        }
+    }
+
+	@Bean
+	@GRpcGlobalInterceptor
+	ServerInterceptor transmitStatusRuntimeExceptionInterceptor() {
+		return TransmitStatusRuntimeExceptionInterceptor.instance();
+	}
 }
+
--- front-service/src/main/java/com/example/service/front/FrontServiceApplication.java
+++ front-service/src/main/java/com/example/service/front/FrontServiceApplication.java
@@ -1,14 +1,26 @@
 package com.example.service.front;
 
+import com.google.protobuf.Any;
+import com.google.protobuf.Int32Value;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.rpc.BadRequest;
+import com.google.rpc.Code;
+import com.google.rpc.Status;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import io.grpc.examples.helloworld.GreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.protobuf.StatusProto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 @SpringBootApplication
 @RestController
@@ -18,18 +30,92 @@ public class FrontServiceApplication {
         SpringApplication.run(FrontServiceApplication.class, args);
     }
 
+
     @GetMapping("/hello")
     String hello() {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565)
+        return call("Tom");
+    }
+
+    @GetMapping("/400")
+    String m400() {
+        return call("400");
+    }
+
+    @GetMapping("/404")
+    String m404() {
+        return call("404");
+    }
+
+    @GetMapping("/500")
+    String m500() {
+        return call("500");
+    }
+
+    @GetMapping("/native")
+    String nativeRuntimeException() {
+        return call("native");
+    }
+
+    String call(String number) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:6565")
                 .usePlaintext(true)
                 .build();
 
         GreeterGrpc.GreeterBlockingStub blockingStub = GreeterGrpc.newBlockingStub(channel);
 
         HelloRequest request = HelloRequest.newBuilder()
-                .setName("Tom")
+                .setName(number)
+                .setAge(Int32Value.newBuilder().setValue(10).build())
                 .build();
 
-        return blockingStub.sayHelloUnary(request).toString();
+        try {
+            HelloReply helloReply = blockingStub.sayHello(request);
+            return new User(helloReply.getMessage()).toString();
+        } catch (StatusRuntimeException e) {
+            Status status = StatusProto.fromThrowable(e);
+            if (status == null) {
+                throw e;
+            }
+
+            switch (status.getCode()) {
+                case Code.INVALID_ARGUMENT_VALUE:
+                    return status.getDetailsList().stream()
+                            .filter(detail -> detail.is(BadRequest.class))
+                            .flatMap(this::messages)
+                            .collect(toList()).toString();
+
+                case Code.NOT_FOUND_VALUE:
+                    throw e;
+
+                case Code.INTERNAL_VALUE:
+                    throw e;
+
+                default:
+                    throw e;
+            }
+        }
+    }
+
+    private Stream<String> messages(Any detail) {
+        try {
+            return detail.unpack(BadRequest.class).getFieldViolationsList().stream()
+                    .map(BadRequest.FieldViolation::getDescription);
+        } catch (InvalidProtocolBufferException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class User {
+    String name;
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
